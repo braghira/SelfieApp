3f7e68ddc7923999eb220159e0d16b2cda13b67d
braghira <andrea.venturoli5@studio.unibo.it>
2024-04-24 15:29:54 +0200

    Start tutorial

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 // config method will attach .env variables to the "process" global variable
 require("dotenv").config();
-console.log(process.env);   // remove this when you're sure it works
+// console.log(process.env);   // remove this when you're sure it works
 // npm modules
+const fs = require('fs');
 const express = require("express");
 const path = require('path');
 const mongoose = require("mongoose");
@@ -9,8 +10,25 @@ const cors = require("cors");
 // our modules
 const workoutRoutes = require("./routes/workouts");
 const utils = require("./utils/environmentDetector");
-// useful variables
-const mongouri = process.env.DB_URI;
+// utilities
+if (process.env) {
+    console.log("environment è dev");    
+    global.port = process.env.PORT;
+    global.mongouri = process.env.DB_URI;
+    global.env = process.env.NODE_ENV;
+} else {
+    console.log("environment è prod");
+    // Leggi il file di configurazione
+    const configFilePath = "config.json";
+    const configData = fs.readFileSync(configFilePath, "utf-8");
+    const config = JSON.parse(configData);
+
+    // Usa le variabili d'ambiente dal file di configurazione
+    global.port = config.PORT;
+    global.mongouri = config.MONGO_URI;
+    global.env = config.NODE_ENV;
+}
+
 const appPath = path.resolve(__dirname, '..', 'selfie', 'dist');
 
 // express app
@@ -24,7 +42,7 @@ app.use((req, res, next) => {
     next();
 });
 
-if (process.env.NODE_ENV === "production") {
+if (global.env === "production") {
     // serve static react files after building the app
     app.use(express.static(appPath));
 }
@@ -32,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
 // routes
 app.use("/api/workouts", workoutRoutes);
 
-if (process.env.NODE_ENV === "production") {
+if (global.env === "production") {
     // route fallback: reindirizza tutte le altre richieste all'app React
     app.get("*", (req, res) => {
         res.sendFile(path.join(appPath, 'index.html'));
@@ -41,12 +59,12 @@ if (process.env.NODE_ENV === "production") {
 
 // connect to db
 mongoose
-    .connect(mongouri, { dbName: "SelfieDB" })
+    .connect(global.mongouri, { dbName: "SelfieDB" })
     .then(() => {
         // listen for requests
-        app.listen(process.env.PORT || 8000, () => {
+        app.listen(global.port || 8000, () => {
             console.log(
-                `DB connected and listening on port ${process.env.PORT}`
+                `DB connected and listening on port ${global.port}`
             );
         });
     })
